9cf5636e88d6e27a495e56fb4a17ad513e45cab0
Park Seo Yeon <syss220211@gmail.com>
2024-12-10 21:27:17 +0900

    [Feat] 월간/주간 과거 일기 기록이 없는 경우에 사용하는 공통 컴포넌트 제작완료
    버튼 tapped를 통해서 액션 연결

---
+++ Projects/Feature/Scene/Home/Sources/Calendar/PastNoneRecordComponentView.swift
@@ -0,0 +1,21 @@
+//
+//  PastNoneRecordComponentView.swift
+//  Home
+//
+//  Created by 박서연 on 2024/12/10.
+//  Copyright © 2024 inner-dev. All rights reserved.
+//
+
+import UIKit
+
+class PastNoneRecordComponentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
